768404f096f484d3fed8e1e821e0471e3d74e7ba
Ousikai <mtfallsVR@protonmail.com>
2022-02-17 12:28:55 -0700

    Create Connect Wallet Button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,184 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+// Blockchain Imports 
+// import { ethers } from 'ethers';
+
+// Custom Components
+import LoadingIndicator from './components/LoadingIndicator';
+
+// Constants
+import twitterLogo from './assets/twitter-logo.svg';
+const TWITTER_HANDLE = 'ColreaATOS';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
 function App() {
+  // State
+  const [isCorrectNetwork, setIsCorrectNetwork] = useState(null);
+  const [currentAccount, setCurrentAccount] = useState(null);
+  // const [location, setLocation] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!'); // DEBUG
+        return;
+      } else {
+        // console.log('We have the ethereum object', ethereum); // DEBUG
+
+        /*
+         * Check if we are on the correct network verison
+         */
+        console.log(ethereum);
+        const networkVersion = await ethereum.request({ method: 'eth_chainId' });
+        console.log(networkVersion);
+        if (networkVersion === '0x13881') { // 0x89 is Matic Mainnet, 0x13881 is Mumbai Testnet
+          // console.log('Client connected to CORRECT Network Version: ' + networkVersion); // DEBUG
+          setIsCorrectNetwork(true);
+        } else {
+          // console.log('Client connected to INCORRECT Network Version: ' + networkVersion); // DEBUG
+          setIsCorrectNetwork(false);
+          setIsLoading(false);
+        }
+
+        /*
+         * Check if we're authorized to access the user's wallet
+         */
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+        console.log(accounts);
+        /*
+         * User can have multiple authorized accounts, we grab the first one if its there!
+         */
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          // console.log('Found an authorized account:', account); // DEBUG
+          setCurrentAccount(account);
+        } else {
+          // console.log('No authorized account found'); // DEBUG
+          setIsLoading(false);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+
+      /*
+       * Fancy method to request access to account.
+       */
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      /*
+       * Boom! This should print out public address once we authorize Metamask.
+       */
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    checkIfWalletIsConnected();
+  }, []);
+
+  // Render Methods
+  const renderContent = () => {
+    // console.log("Location:" + location); // DEBUG
+    if (isLoading) {
+      return <LoadingIndicator />;
+    }  
+
+    /*
+    * Scenario #0: Wrong Network Not Connected
+    */
+    if (!isCorrectNetwork) {
+      return (
+        <div className="select-character-container">
+          <p className="sub-text">You are on the wrong network! Please connect to <b>Polygon</b> and refresh to continue.</p>
+          <img
+            src="https://mojo.dailybruin.com/wp-content/uploads/2014/06/kirito-gif-kirito-sword-art-online-34958972-500-281.gif"
+            alt="Sword Art Online Menu GIF"
+          />
+        </div>
+      );
+    }
+
+    /*
+    * Scenario #1: Account Not Connected
+    */
+    else if (!currentAccount) {
+      return (
+        <div className="connect-wallet-container">
+          <p className="sub-text">Mint your Metaverse ID Card!</p>
+          <img
+            src="https://i.pinimg.com/originals/31/7e/75/317e756a8c338c113cd328e05a1e2713.gif"
+            alt="Sword Art Online Gif"
+          />
+          <button
+            className="cta-button connect-wallet-button"
+            onClick={connectWalletAction}
+          >
+            Connect Wallet To Get Started
+          </button>
+        </div>
+      );
+    }
+    // /*
+    // * Scenario #2: Connected Wallet but no Player NFT
+    // */
+    // else if (location === "MintCharacter") {
+    //     return <MintCharacter setLocation={setLocation} />;
+    // }
+    // /*
+    // * Scenario #3: If there is a connected wallet and list of characters and NO selected charaters,
+    // * show your list of characters to select!
+    // */
+    // else if (location === "SelectCharacter") {
+    //   return <SelectCharacter characterList={characterList} setCharacterNFT={setCharacterNFT} setLocation={setLocation}  />;
+    // }
+    // /*
+    // * Scenario #4: If there is a connected wallet and selected characterNFT, it's time to battle!
+    // */
+    // else if (location === "ExploreWasteland") {
+    //     return <ExploreWasteland characterNFT={characterNFT} setLocation={setLocation} />;
+    // }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Navbar setLocation={setLocation}/> */}
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">Colrea</p>
+          {renderContent()}
+        </div>
+        <br/>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`Twitter -> @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
     </div>
   );
 }
