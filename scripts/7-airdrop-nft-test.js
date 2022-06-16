7c0a8a25bf29e5883fe9827e55cacdd172e425e9
OscarKevinXR <mtfallsVR@protonmail.com>
2022-06-15 21:27:01 -0400

    adk SDK scripts pt2

--- scripts/7-airdrop-nft-test.js
+++ scripts/7-airdrop-nft-test.js
@@ -1,22 +1,29 @@
 import sdk from "./1-initialize-sdk.js";
 
-// This is the address to our ERC-1155 membership NFT contract.
-const contract = sdk.getEditionDrop(
-  "0xF39C6Fa1D6cBE05De22f5eE8a6eB63Af834750Dd",
-);
-
-const walletAddresses =[
-  "0x0FeF592988529Fd2dcdbE9ef6fBf3Bb6A0A21066",
-  "0x01ab57945AA1dFb9444dD0e2611Cf0C7Ff97caf1",
-];
-
-const tokenId = "0";
-
 (async () => {
   try {    
+    // This is the address to our ERC-1155 membership NFT contract.
+    const contract = sdk.getEditionDrop(
+      "0xF39C6Fa1D6cBE05De22f5eE8a6eB63Af834750Dd",
+    );
+
+    const addresses = [
+      {
+        address: "0x0FeF592988529Fd2dcdbE9ef6fBf3Bb6A0A21066",
+        quantity: 2,
+      },
+      {
+      address: "0x01ab57945AA1dFb9444dD0e2611Cf0C7Ff97caf1",
+        quantity: 3,
+      },
+    ];
+
+    const tokenId = "0";
     // Call transferBatch on all our airdrop targets.
     console.log("🌈 Starting airdrop...")
-    await contract.airdrop(walletAddresses, tokenId);
+    console.log(addresses)
+    console.log(tokenId)
+    await contract.airdrop(tokenId, addresses);
     console.log("✅ Successfully airdropped tokens to all the holders of the NFT!");
   } catch (err) {
     console.error("Failed to airdrop tokens", err);
